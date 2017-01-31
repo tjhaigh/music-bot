170f872d4df52f061f40222388ce683f746e5c40
Trevor Haigh <tjhaigh95@gmail.com>
2017-01-31 19:19:29 +0000

    update

--- music-bot.py
+++ music-bot.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python3
 
-print('test')
+import discord
+import asyncio
+import httplib2
+import os
+import ConfigParser
+
+
+# Get keys from config file
+config = ConfigParser.ConfigParser()
+config.readfp(open('config.ini'))
+TOKEN = config.get('Keys', 'token')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('.test'):
+        await client.send_message(message.channel, 'Here is a test message')
+
+def main():
+    client.run(TOKEN)
+
+if __name__ == '__main__':
+    main()
